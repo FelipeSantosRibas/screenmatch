6319d26ec96ec4399736373404976380879d982e
FelipeSantosRibas <felipesantosribas2007@gmail.com>
2026-03-15 15:25:15 -0300

    Implemented deserializing from Json to DadosSerie using Jackson, extracting from Ombd Api

---
+++ src/main/java/br/com/alura/screenmatch/service/ConsumoAPI.java
@@ -0,0 +1,29 @@
+package br.com.alura.screenmatch.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoAPI {
+
+    public String obterDados(String endereco) {
+        HttpClient client = HttpClient.newHttpClient(); // Cria um cliente HTTP
+        HttpRequest request = HttpRequest.newBuilder() // Define qual endereço será requisitado
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString()); // Faz a requisição e armazena no response
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String json = response.body();
+        return json;
+    }
+}
