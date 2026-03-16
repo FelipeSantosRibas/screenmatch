8c8efab2d8530d0c9fef7906544421b18ba65ef9
FelipeSantosRibas <felipesantosribas2007@gmail.com>
2026-03-16 16:20:15 -0300

    Added desserializing for Episodes and Seasons

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,17 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverteDados;
+import ch.qos.logback.core.encoder.ByteArrayUtil;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -16,12 +22,30 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		// Obtém dados do fimle
 		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=one%20piece&apikey=6585022c");
+		var json = consumoAPI.obterDados("http://www.omdbapi.com/?t=rick+and+morty&apikey=6585022c");
 		System.out.println(json);
 
+		// Converte desserializa dados
 		ConverteDados conversor = new ConverteDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+
+		// Obtém e desserializa dados do episódio
+		json = consumoAPI.obterDados("https://www.omdbapi.com/?t=rick+and+morty&season=1&episode=1&apikey=6585022c");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		// Temporadas
+		for (int i = 1; i <= dados.totalTemporadas(); i++){
+			json = consumoAPI.obterDados("https://www.omdbapi.com/?t=rick+and+morty&season="+i+"&apikey=6585022c");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			System.out.println(dadosTemporada);
+			temporadas.add(dadosTemporada);
+		}
 	}
 }
